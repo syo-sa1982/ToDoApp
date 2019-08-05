da2ab0051e0c1f987a7ac1a21ccbdea33b91d2fa
YosukeFujii <fujii1308@gmail.com>
2019-08-06 02:38:06 +0900

    AndroidX化とDeployGate導入

--- app/src/main/java/com/syousa1982/todoapp/MainActivity.kt
+++ app/src/main/java/com/syousa1982/todoapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.syousa1982.todoapp
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
